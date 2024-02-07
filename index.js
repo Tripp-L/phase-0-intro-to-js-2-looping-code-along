c798fe9b616d3346eb19b85d3bbb68a7053db97f
William Laursen <Tlaursen8@yahoo.com>
2024-02-07 14:47:06 -0500

    Looping Code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const messagesArray = []
+  for (let i = 0; i < names.length; i++) {
+    messagesArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+  return messagesArray
+}
+function countDown() {
+  let startingNumber = 10
+  while (startingNumber > -1) {
+    console.log(startingNumber--)
+  }
+}
