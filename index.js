e46d4edb8a620679ba2215abd71db7e9f330975a
trippl <tlaursen8@yahoo.com>
2024-04-08 20:03:29 -0400

    JS looping code along

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+// const gifts = ["teddy bear", "drone", "doll"]
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) { // starts i at 0 because arrays have zero-based indexes.
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//     debugger
+// }
+
+//   return gifts
+// }
+
+// wrapGifts(gifts)
+
+
+function writeCards(names, event) {
+
+  const messagesArr = []
+  for (let i = 0; i < names.length; i++) {
+  messagesArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  debugger
+}
+
+return messagesArr
+}
+
+function countDown() {
+  let countDown = 10
+  while (countDown > -1) {
+    console.log(countDown--)
+  }
+}
+
+/* Original code:
 function writeCards(names, event) {
   const messagesArray = []
   for (let i = 0; i < names.length; i++) {
@@ -11,3 +44,4 @@ function countDown() {
     console.log(startingNumber--)
   }
 }
+*/
